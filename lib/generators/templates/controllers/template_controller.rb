dc2f4014385749fb3c3d3ccb5ce3f6ce46ef6584
ivan Lan <mumumumushu@gmail.com>
2017-08-26 19:54:41 +0800

    User class itself instead of class_name as the attr of mehod Shotengai::Controller::Base.resources=

--- lib/generators/templates/controllers/template_controller.rb
+++ lib/generators/templates/controllers/template_controller.rb
@@ -1,3 +1,3 @@
 class <%= @controller_prefix %><%= @klass_name.pluralize %>Controller < Shotengai::Controller::<%= "#{@role}/#{@key}".camelize %>Controller
-  self.resources = '<%= @klass_name %>'
+  self.resources = <%= @klass_name %>
 end
