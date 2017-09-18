b810f251d12d354632e53c047b6e2994a69cd828
ivan Lan <mumumumushu@gmail.com>
2017-09-18 20:29:12 +0800

    Improve join_catalog_system with a new option :validate_name_chain

--- lib/shotengai/product.rb
+++ lib/shotengai/product.rb
@@ -107,9 +107,10 @@ module Shotengai
         catalog_class = catalog_class_name.constantize
         tag_name = options[:as] || catalog_class.model_name.singular
         acts_as_taggable_on tag_name.to_sym
+        # set { validate_name_chain: false } to skip the validate_name_chain
         # 只有完整替换(只属于一个分类)的时候才进行验证，add remove 暂时未添加
         # Just catalogs_list= have a validation
-        class_eval do
+        options[:validate_name_chain].eql?(false) || class_eval do
           define_method("#{tag_name}_list=") { |value|
             super catalog_class.validate_name_chain(value)
           }
