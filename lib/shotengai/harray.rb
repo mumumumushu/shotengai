91aac2174ed53f288420fb1df4ea6dd49f2fde24
ivan Lan <mumumumushu@gmail.com>
2017-10-13 14:19:20 +0800

    Improve Shotengai::Harray

--- lib/shotengai/harray.rb
+++ lib/shotengai/harray.rb
@@ -1,11 +1,15 @@
 module Shotengai
   class Harray < Array
     def keys
-      self.map { |obj| obj['key'] || obj[:ket] }
+      self.map { |obj| obj['key'] }
     end
 
     def vals
-      self.map { |obj| obj['key'] || obj[:ket] }
+      self.map { |obj| obj['key'] }
+    end
+
+    def val_at key
+      self.bsearch { |obj| obj['key'].eql?(key) }&.[]('val')
     end
   end
 end
